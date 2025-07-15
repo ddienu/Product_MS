e375622d69f8c5df58813136ce1aff57d7ffe919
ddienu <rockdann@hotmail.com>
2025-07-15 16:20:49 -0500

    feat: Mapper implementation to avoid boiler plate and DRY principle

--- Product-MS/src/main/java/com/diegonunez/Product_MS/mapper/ProductJsonApiMapper.java
+++ Product-MS/src/main/java/com/diegonunez/Product_MS/mapper/ProductJsonApiMapper.java
@@ -1,2 +1,21 @@
-package com.diegonunez.Product_MS.mapper;public class ProductJsonApiMapper {
+package com.diegonunez.Product_MS.mapper;
+
+import com.diegonunez.Product_MS.dto.jsonapi.ProductResponseJsonApi;
+import com.diegonunez.Product_MS.entity.Product;
+
+public class ProductJsonApiMapper {
+
+    private ProductJsonApiMapper() {
+        throw new UnsupportedOperationException("Utility class");
+    }
+
+    public static ProductResponseJsonApi toJsonApi(Product product){
+        return new ProductResponseJsonApi(
+                product.getId(),
+                product.getName(),
+                product.getPrice()
+        );
+    }
+
+
 }
